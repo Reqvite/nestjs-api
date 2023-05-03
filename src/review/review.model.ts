097ceddf7eb11e87cd8894d5152ad4d615296557
Reqvite <reqvite2233@gmail.com>
2023-05-03 22:41:17 +0300

    add review model, controller, service

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,8 +1,23 @@
-export class ReviewModel {
-  _id: string;
+import { prop } from '@typegoose/typegoose';
+import { TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+import { Types } from 'mongoose';
+
+export class ReviewModel extends TimeStamps {
+  @prop()
+  _id: Types.ObjectId;
+
+  @prop()
   name: string;
+
+  @prop()
   title: string;
+
+  @prop()
   description: string;
+
+  @prop()
   rating: number;
-  createAt: Date;
+
+  @prop()
+  productId: Types.ObjectId;
 }
